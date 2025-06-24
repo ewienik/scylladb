a65c9e7b270849193c4bc788cfd4cd737a69cf02
Pawel Pery <pawel.pery@scylladb.com>
2025-06-24 15:43:53 +0200

    vector_store_client: implement ip addr retrieval from dns
    This patch is a part of vector_store_client sharded service
    implementation for a communication with vector-store service.
    
    It implements functionality for refreshing ip address of the
    vector-store service dns name and creating a new HTTP client with that
    address. It also provides cleanup of unused http clients. There is
    hardcoded frequency limit of refreshing dns address.
    
    Reference: VS-47 VS-45

--- service/vector_store_client.cc
+++ service/vector_store_client.cc
@@ -11,12 +11,28 @@
 #include "exceptions/exceptions.hh"
 #include <charconv>
 #include <regex>
+#include <seastar/http/client.hh>
+#include <seastar/net/dns.hh>
+#include <seastar/net/inet_address.hh>
 
 namespace {
 
 using configuration_exception = exceptions::configuration_exception;
 using host_name = service::vector_store_client::host_name;
+using http_client = http::experimental::client;
+using inet_address = seastar::net::inet_address;
+using milliseconds = std::chrono::milliseconds;
 using port_number = service::vector_store_client::port_number;
+using time_point = lowres_clock::time_point;
+
+// Minimum interval between cleanup tasks
+constexpr auto CLEANUP_INTERVAL = std::chrono::seconds(5);
+
+// Minimum interval between dns name refreshes
+constexpr auto DNS_REFRESH_INTERVAL = std::chrono::seconds(5);
+
+/// Timeout for waiting for a new client to be available
+constexpr auto WAIT_FOR_CLIENT_TIMEOUT = std::chrono::seconds(5);
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 logging::logger vslogger("vector_store_client");
@@ -51,13 +67,147 @@ auto parse_service_uri(std::string_view uri) -> std::optional<std::tuple<host_na
 namespace service {
 
 struct vector_store_client::impl {
+    lw_shared_ptr<http_client> current_client;           ///< The current http client
+    std::vector<lw_shared_ptr<http_client>> old_clients; ///< The current http client
     host_name host;                                      ///< The host name for the vector-store service.
     port_number port{};                                  ///< The port number for the vector-store service.
+    inet_address addr;                                   ///< The address for the vector-store service.
+    time_point last_dns_refresh;                         ///< The last time the DNS service was refreshed to get the vector-store service address.
     gate tasks_gate;                                     ///< Gate to control tasks
+    condition_variable refresh_cv;                       ///< Condition variable to signal that the service address should be refreshed.
+    condition_variable refresh_client_cv;                ///< Condition variable to signal that a new client is available.
+    condition_variable refresh_stop_cv;                  ///< Condition variable to wait for the refresh task to stop.
+    condition_variable cleanup_stop_cv;                  ///< Condition variable to wait for the cleanup task to stop.
+    bool stop_refreshing{};                              ///< Flag to stop refreshing the service address.
+    milliseconds dns_refresh_interval = DNS_REFRESH_INTERVAL;
+    milliseconds wait_for_client_timeout = WAIT_FOR_CLIENT_TIMEOUT;
+    std::function<future<inet_address>(sstring const&)> dns_resolver;
 
     impl(host_name host_, port_number port_)
         : host(std::move(host_))
-        , port(port_) {
+        , port(port_)
+        , dns_resolver([](auto const& host) {
+            return net::dns::resolve_name(host);
+        }) {
+    }
+
+    auto refresh_addr() -> future<> {
+        auto new_addr = inet_address{};
+        try {
+            new_addr = co_await dns_resolver(host);
+        } catch (std::system_error const&) {
+            // addr should be empty if the resolution failed
+        }
+        if (new_addr.is_addr_any()) {
+            current_client = nullptr;
+            co_return;
+        }
+
+        // Check if the new address is the same as the current one
+        if (current_client && new_addr == addr) {
+            co_return;
+        }
+
+        addr = new_addr;
+        old_clients.emplace_back(current_client);
+        current_client = make_lw_shared<http_client>(socket_address(addr, port));
+    }
+
+    auto refresh_stop_wait(time_point timeout) -> future<> {
+        try {
+            co_await refresh_stop_cv.wait(timeout);
+        } catch (condition_variable_timed_out&) {
+        }
+    }
+
+    /// Refresh the vector store service IP address from the dns name. Returns true if the address was refreshed.
+    auto refresh_addr_task() -> future<> {
+        auto requested_refresh = false;
+        for (;;) {
+            if (stop_refreshing) {
+                break;
+            }
+
+            // Do not refresh the service address too often
+            auto now = lowres_clock::now();
+            if (now - last_dns_refresh > dns_refresh_interval) {
+                last_dns_refresh = now;
+                co_await refresh_addr();
+            }
+
+            if (requested_refresh || !current_client) {
+                // Wait till the end of the refreshing interval
+                co_await refresh_stop_wait(last_dns_refresh + dns_refresh_interval);
+                continue;
+            }
+
+            if (stop_refreshing) {
+                break;
+            }
+
+            // new client is available
+            refresh_client_cv.broadcast();
+
+            requested_refresh = false;
+            co_await refresh_cv.when();
+            requested_refresh = true;
+        }
+    }
+
+    void trigger_dns_refresh() {
+        refresh_cv.signal();
+    }
+
+    auto cleanup() -> future<> {
+        for (auto& client : old_clients) {
+            if (client && client.owned()) {
+                co_await client->close();
+                client = nullptr;
+            }
+        }
+        std::erase_if(old_clients, [](auto const& client) {
+            return !client;
+        });
+    }
+
+    /// Refresh the vector store service IP address from the dns name. Returns true if the address was refreshed.
+    auto cleanup_task() -> future<> {
+        for (;;) {
+            try {
+                co_await cleanup_stop_cv.wait(lowres_clock::now() + CLEANUP_INTERVAL);
+                break;
+            } catch (condition_variable_timed_out&) {
+            }
+            co_await cleanup();
+        }
+        co_await cleanup();
+        if (current_client) {
+            co_await current_client->close();
+        }
+        current_client = nullptr;
+    }
+
+    struct get_client_response {
+        lw_shared_ptr<http_client> client; ///< The http client.
+        host_name host;                    ///< The host name for the vector-store service.
+    };
+
+    auto get_client() -> future<std::expected<get_client_response, addr_unavailable>> {
+        if (current_client) {
+            co_return get_client_response{.client = current_client, .host = host};
+        }
+
+        trigger_dns_refresh();
+
+        try {
+            co_await refresh_client_cv.wait(lowres_clock::now() + wait_for_client_timeout);
+        } catch (condition_variable_timed_out&) {
+        }
+
+        if (!current_client || stop_refreshing) {
+            co_return std::unexpected{addr_unavailable{}};
+        }
+        co_return get_client_response{.client = current_client, .host = host};
     }
 };
 
@@ -84,12 +234,31 @@ auto vector_store_client::start_background_tasks() -> future<> {
     if (is_disabled()) {
         co_return;
     }
+
+    /// start the background task to refresh the service address
+    (void)try_with_gate(_impl->tasks_gate, [this] {
+        return _impl->refresh_addr_task();
+    }).handle_exception([](std::exception_ptr eptr) {
+        vslogger.error("Failed to start a Vector Store Client refresh task: {}", eptr);
+    });
+
+    /// start the background task to cleanup
+    (void)try_with_gate(_impl->tasks_gate, [this] {
+        return _impl->cleanup_task();
+    }).handle_exception([](std::exception_ptr eptr) {
+        vslogger.error("Failed to start a Vector Store Client cleanup task: {}", eptr);
+    });
 }
 
 auto vector_store_client::stop() -> future<> {
     if (is_disabled()) {
         co_return;
     }
+
+    _impl->stop_refreshing = true;
+    _impl->refresh_stop_cv.signal();
+    _impl->refresh_cv.signal();
+    _impl->cleanup_stop_cv.signal();
     co_await _impl->tasks_gate.close();
 }
 
@@ -105,6 +274,41 @@ auto vector_store_client::port() const -> std::expected<port_number, disabled> {
         return std::unexpected{disabled{}};
     }
     return {_impl->port};
+}
+
+void vector_store_client_tester::set_dns_refresh_interval(vector_store_client& vsc, std::chrono::milliseconds interval) {
+    if (vsc.is_disabled()) {
+        on_internal_error(vslogger, "Cannot set dns_refresh_interval on a disabled vector store client");
+    }
+    vsc._impl->dns_refresh_interval = interval;
+}
+
+void vector_store_client_tester::set_wait_for_client_timeout(vector_store_client& vsc, std::chrono::milliseconds timeout) {
+    if (vsc.is_disabled()) {
+        on_internal_error(vslogger, "Cannot set wait_for_client_timeout on a disabled vector store client");
+    }
+    vsc._impl->wait_for_client_timeout = timeout;
+}
+
+void vector_store_client_tester::set_dns_resolver(vector_store_client& vsc, std::function<future<inet_address>(sstring const&)> resolver) {
+    if (vsc.is_disabled()) {
+        on_internal_error(vslogger, "Cannot set dns_resolver on a disabled vector store client");
+    }
+    vsc._impl->dns_resolver = std::move(resolver);
+}
+
+void vector_store_client_tester::trigger_dns_resolver(vector_store_client& vsc) {
+    if (vsc.is_disabled()) {
+        on_internal_error(vslogger, "Cannot trigger a dns resolver on a disabled vector store client");
+    }
+    vsc._impl->trigger_dns_refresh();
+}
+
+auto vector_store_client_tester::can_resolve_hostname(vector_store_client& vsc) -> future<bool> {
+    if (vsc.is_disabled()) {
+        on_internal_error(vslogger, "Cannot check hostname resolving on a disabled vector store client");
+    }
+    co_return co_await vsc._impl->get_client();
 }
 
 } // namespace service
--- test/boost/vector_store_client_test.cc
+++ test/boost/vector_store_client_test.cc
@@ -26,6 +26,8 @@ using vector_store_client = service::vector_store_client;
 using vector_store_client_tester = service::vector_store_client_tester;
 using config = vector_store_client::config;
 using configuration_exception = exceptions::configuration_exception;
+using inet_address = seastar::net::inet_address;
+using port_number = vector_store_client::port_number;
 
 } // namespace
 
@@ -59,6 +61,105 @@ BOOST_AUTO_TEST_CASE(vector_store_client_test_ctor) {
         BOOST_CHECK_THROW(vector_store_client{cfg}, configuration_exception);
         cfg.vector_store_uri.set("http://authority.com:6080/bad/path");
         BOOST_CHECK_THROW(vector_store_client{cfg}, configuration_exception);
+    }
+}
+
+SEASTAR_TEST_CASE(vector_store_client_test_dns_refresh) {
+    auto cfg = config();
+    cfg.vector_store_uri.set("http://good.authority.here:6080");
+
+    {
+        // Resolving of the hostname is started in start_background_tasks()
+        auto vs = vector_store_client{cfg};
+        BOOST_CHECK(!vs.is_disabled());
+
+        vector_store_client_tester::set_dns_refresh_interval(vs, std::chrono::milliseconds(2000));
+        vector_store_client_tester::set_wait_for_client_timeout(vs, std::chrono::milliseconds(100));
+        vector_store_client_tester::set_dns_resolver(vs, [](auto const& host) -> future<inet_address> {
+            BOOST_CHECK_EQUAL(host, "good.authority.here");
+            co_return inet_address("127.0.0.1");
+        });
+
+        co_await vs.start_background_tasks();
+
+        BOOST_CHECK(co_await vector_store_client_tester::can_resolve_hostname(vs));
+
+        co_await vs.stop();
+    }
+
+    {
+        // Unable to resolve the hostname
+        auto vs = vector_store_client{cfg};
+        BOOST_CHECK(!vs.is_disabled());
+
+        vector_store_client_tester::set_dns_refresh_interval(vs, std::chrono::milliseconds(2000));
+        vector_store_client_tester::set_wait_for_client_timeout(vs, std::chrono::milliseconds(100));
+        vector_store_client_tester::set_dns_resolver(vs, [](auto const& host) -> future<inet_address> {
+            BOOST_CHECK_EQUAL(host, "good.authority.here");
+            throw std::system_error();
+            co_return inet_address("127.0.0.1");
+        });
+
+        co_await vs.start_background_tasks();
+
+        BOOST_CHECK(!co_await vector_store_client_tester::can_resolve_hostname(vs));
+
+        co_await vs.stop();
+    }
+
+    {
+        // Resolving of the hostname is repeated after errors
+        auto vs = vector_store_client{cfg};
+        BOOST_CHECK(!vs.is_disabled());
+
+        vector_store_client_tester::set_dns_refresh_interval(vs, std::chrono::milliseconds(100));
+        vector_store_client_tester::set_wait_for_client_timeout(vs, std::chrono::milliseconds(500));
+        auto count = 0;
+        vector_store_client_tester::set_dns_resolver(vs, [&count](auto const& host) -> future<inet_address> {
+            BOOST_CHECK_EQUAL(host, "good.authority.here");
+            if (count++ < 2) {
+                throw std::system_error();
+            }
+            co_return inet_address("127.0.0.1");
+        });
+
+        co_await vs.start_background_tasks();
+
+        BOOST_CHECK(co_await vector_store_client_tester::can_resolve_hostname(vs));
+
+        co_await vs.stop();
+    }
+
+    {
+        // Minimal interval between DNS refreshes is respected
+        auto vs = vector_store_client{cfg};
+        BOOST_CHECK(!vs.is_disabled());
+
+        vector_store_client_tester::set_dns_refresh_interval(vs, std::chrono::milliseconds(10));
+        vector_store_client_tester::set_wait_for_client_timeout(vs, std::chrono::milliseconds(100));
+        auto count = 0;
+        vector_store_client_tester::set_dns_resolver(vs, [&count](auto const& host) -> future<inet_address> {
+            BOOST_CHECK_EQUAL(host, "good.authority.here");
+            count++;
+            co_return inet_address("127.0.0.1");
+        });
+
+        co_await vs.start_background_tasks();
+
+        BOOST_CHECK(co_await vector_store_client_tester::can_resolve_hostname(vs));
+        BOOST_CHECK_EQUAL(count, 1);
+        count = 0;
+        vector_store_client_tester::trigger_dns_resolver(vs);
+        vector_store_client_tester::trigger_dns_resolver(vs);
+        vector_store_client_tester::trigger_dns_resolver(vs);
+        vector_store_client_tester::trigger_dns_resolver(vs);
+        vector_store_client_tester::trigger_dns_resolver(vs);
+        co_await sleep(std::chrono::milliseconds(20)); // wait for the next DNS refresh
+        BOOST_CHECK(co_await vector_store_client_tester::can_resolve_hostname(vs));
+        BOOST_CHECK_GE(count, 1);
+        BOOST_CHECK_LE(count, 2);
+
+        co_await vs.stop();
     }
 }
 
